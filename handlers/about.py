ff03ed895c5dc9df788aaf74fade32c9b12fd768
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-06-25 02:03:32 +0530

    fix: added photo to about

--- handlers/about.py
+++ handlers/about.py
@@ -1,9 +1,9 @@
-from misc.text import repo_link
+from misc.text import repo_link, image_link
 from telegram.ext import CommandHandler
 
 
 def about_website(update, context):
-    update.message.reply_text(repo_link)
+    update.message.reply_photo(image_link, caption=repo_link)
 
 
 about_handler = CommandHandler("about", about_website)
